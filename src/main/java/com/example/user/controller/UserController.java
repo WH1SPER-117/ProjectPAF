da99dddb208c4e312073b6ff6c35f5185a2db300
Hasvin Kenula <131936695+WH1SPER-117@users.noreply.github.com>
2025-04-27 22:41:15 +0530

    first commit

---
+++ src/main/java/com/example/user/controller/UserController.java
@@ -0,0 +1,29 @@
+package com.example.user.controller;
+
+import com.example.user.model.User;
+import com.example.user.service.UserService;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/users")
+public class UserController {
+    @Autowired
+    private UserService userService;
+
+    @PostMapping
+    public ResponseEntity<User> createUser(@RequestBody User user) {
+        User createdUser = userService.createUser(user);
+        return ResponseEntity.ok(createdUser);
+    }
+
+    @GetMapping
+    public ResponseEntity<List<User>> getAllUsers() {
+        List<User> users = userService.getAllUsers();
+        return ResponseEntity.ok(users);
+    }
+}
