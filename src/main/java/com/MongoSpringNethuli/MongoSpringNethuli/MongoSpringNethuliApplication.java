761093cd6417de909e00efd9ad9948a3efac1df0
nethuli1030 <nethulihimallikarachchi@gmail.com>
2025-04-28 19:25:55 +0530

    set up main class

---
+++ src/main/java/com/MongoSpringNethuli/MongoSpringNethuli/MongoSpringNethuliApplication.java
@@ -0,0 +1,13 @@
+package com.MongoSpringNethuli.MongoSpringNethuli;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MongoSpringNethuliApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MongoSpringNethuliApplication.class, args);
+	}
+
+}
