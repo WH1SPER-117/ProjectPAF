07db1765ad57a92716170b381322c7e086c49c7b
nethuli1030 <nethulihimallikarachchi@gmail.com>
2025-04-28 10:05:18 +0530

    edit changes

--- src/main/java/com/Repository/ProgressRepo.java
+++
@@ -1,8 +0,0 @@
-package com.Repository;
-
-import org.springframework.data.mongodb.repository.MongoRepository;
-import com.ProgressModel.LearningProgress;
-
-public interface ProgressRepo extends MongoRepository<LearningProgress,Integer>{
-
-}
--- src/main/java/com/progressController/progressController.java
+++
@@ -1,5 +0,0 @@
-package com.progressController;
-
-public class progressController {
-
-}
